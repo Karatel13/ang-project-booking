36fa941ef581b6b0d6d40c218455a20e29985be4
Karatel13 <davitgogoladze2000@gmail.com>
2025-04-24 21:45:21 +0400

    V1.213 ReStyle Calendar

--- src/app/Pages/rooms/filter/filter.component.ts
+++ src/app/Pages/rooms/filter/filter.component.ts
@@ -3,53 +3,44 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RoomFilter } from '../../../Models/roomFilter';
 import { UserService } from '../../../Services/get-url.service';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [
-    CommonModule, 
-    FormsModule, 
-    MatDatepickerModule, 
-    MatFormFieldModule, 
-    MatInputModule, 
-    MatNativeDateModule
-  ],
+  imports: [CommonModule, FormsModule],
   templateUrl: './filter.component.html',
-  styleUrls: ['./filter.component.scss']
+  styleUrl: './filter.component.scss'
 })
 
 export class FilterComponent {
   constructor(private http: UserService) {}
-
+  
   roomTypeId: number | null = null;
   priceFrom: number = 0;
   priceTo: number = 1000;
   maximumGuests: number | null = null;
-  dateRange: { start: Date | null, end: Date | null } = { start: null, end: null };
+  checkIn: string = '';
+  checkOut: string = '';
   roomTypes: any[] = [];
   guestsOptions: number[] = [1, 2, 3, 4, 5, 6];
 
   @Output() filtersChanged = new EventEmitter<RoomFilter>();
-
+  
   ngOnInit() {
     this.http.getRoomsType().subscribe((data: any) => {
       this.roomTypes = data;
     });
   }
-
   applyFilters() {
     const filter: RoomFilter = {
       roomTypeId: this.roomTypeId ?? 0,
       priceFrom: this.priceFrom,
       priceTo: this.priceTo,
-      maximumGuests: this.maximumGuests ?? 0,
-      dateRange: this.dateRange.start && this.dateRange.end ? this.dateRange : null
+      maximumGuests: this.maximumGuests ?? 0, 
+      checkIn: this.checkIn,
+      checkOut: this.checkOut
     };
     this.filtersChanged.emit(filter);
   }
+  
 }
