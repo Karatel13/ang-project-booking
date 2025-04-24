147426e4715a83d04514728dc34013c93b1138d2
Karatel13 <davitgogoladze2000@gmail.com>
2025-04-24 21:32:53 +0400

    V1.212 ReStyling Fillter

--- src/app/Models/roomFilter.ts
+++ src/app/Models/roomFilter.ts
@@ -1,8 +1,9 @@
 export interface RoomFilter {
-    roomTypeId: number;
-    priceFrom: number;
-    priceTo: number;
-    maximumGuests: number;
-    checkIn: string;
-    checkOut: string;
-  }
+  roomTypeId: number;
+  priceFrom: number;
+  priceTo: number;
+  maximumGuests: number;
+  checkIn?: string;
+  checkOut?: string;
+  dateRange?: { start: Date | null, end: Date | null } | null;
+}
--- src/app/Pages/rooms/filter/filter.component.ts
+++ src/app/Pages/rooms/filter/filter.component.ts
@@ -2,32 +2,53 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RoomFilter } from '../../../Models/roomFilter';
+import { UserService } from '../../../Services/get-url.service';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-filter',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [
+    CommonModule, 
+    FormsModule, 
+    MatDatepickerModule, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatNativeDateModule
+  ],
   templateUrl: './filter.component.html',
-  styleUrl: './filter.component.scss'
+  styleUrls: ['./filter.component.scss']
 })
+
 export class FilterComponent {
-  roomTypeId: number = 0;
+  constructor(private http: UserService) {}
+
+  roomTypeId: number | null = null;
   priceFrom: number = 0;
-  priceTo: number = 0;
-  maximumGuests: number = 0;
-  checkIn: string = '';
-  checkOut: string = '';
+  priceTo: number = 1000;
+  maximumGuests: number | null = null;
+  dateRange: { start: Date | null, end: Date | null } = { start: null, end: null };
+  roomTypes: any[] = [];
+  guestsOptions: number[] = [1, 2, 3, 4, 5, 6];
 
   @Output() filtersChanged = new EventEmitter<RoomFilter>();
 
+  ngOnInit() {
+    this.http.getRoomsType().subscribe((data: any) => {
+      this.roomTypes = data;
+    });
+  }
+
   applyFilters() {
     const filter: RoomFilter = {
-      roomTypeId: this.roomTypeId,
+      roomTypeId: this.roomTypeId ?? 0,
       priceFrom: this.priceFrom,
       priceTo: this.priceTo,
-      maximumGuests: this.maximumGuests,
-      checkIn: this.checkIn,
-      checkOut: this.checkOut
+      maximumGuests: this.maximumGuests ?? 0,
+      dateRange: this.dateRange.start && this.dateRange.end ? this.dateRange : null
     };
     this.filtersChanged.emit(filter);
   }
