adbfd23d99ffda7f15715dddd8a3439cab4ae7ab
Karatel13 <davitgogoladze2000@gmail.com>
2025-05-06 20:22:02 +0400

    V1.231 Add Favorite rooms routing/ Add booking page

--- src/app/Pages/booking/booking.component.ts
+++ src/app/Pages/booking/booking.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { ErrorDialogComponent } from '../../Components/error-dialog/error-dialog.component';
+import { UserService } from '../../Services/get-url.service';
+import { Booking } from '../../Models/booking';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-booking',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.scss']
 })
 export class BookingComponent {
+  constructor(private http: UserService){}
+  bookingArr: Booking[] = [];
+  ngOnInit(){
+    this.http.getAllBooking().subscribe(resp => {
+      console.log(resp);
+      this.displayBookings(resp);
+    });
+  }
+  displayBookings(arr: any){
+    this.bookingArr = arr;
+  }
 }  
--- src/app/Pages/home/favorite-rooms/favorite-rooms.component.ts
+++ src/app/Pages/home/favorite-rooms/favorite-rooms.component.ts
@@ -3,19 +3,21 @@ import { UserService } from '../../../Services/get-url.service';
 import { CommonModule } from '@angular/common';
 import { Rooms } from '../../../Models/rooms';
 import { forkJoin, Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-favorite-rooms',
   templateUrl: './favorite-rooms.component.html',
   styleUrl: './favorite-rooms.component.scss',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   standalone: true
 })
 export class FavoriteRoomsComponent implements OnInit {
   rooms: Rooms[] = [];
   private favoriteRoomIds = [1, 2, 3, 5, 6, 11];
 
-  constructor(private http: UserService) {}
+  constructor(private http: UserService, private router: Router) {}
 
   ngOnInit(): void {
     const requests: Observable<Rooms>[] = this.favoriteRoomIds.map(id =>
@@ -28,8 +30,12 @@ export class FavoriteRoomsComponent implements OnInit {
         console.log(this.rooms);
       },
       (error) => {
-        console.error('Ошибка при загрузке комнат:', error);
+        console.error('Error', error);
       }
     );
+    
+  }
+  navigateToRoomDetails(roomId: number): void {
+    this.router.navigate(['/rooms-details', roomId]);
   }
 }
